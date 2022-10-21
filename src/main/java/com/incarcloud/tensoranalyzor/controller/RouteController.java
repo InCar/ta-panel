2b45afe3d99bec77e072b8ceaae4c410675ef6a7
XGH <ghxiong@incarcloud.com>
2022-10-22 02:01:16 +0800

    fix index.html for webflux

--- src/main/java/com/incarcloud/tensoranalyzor/controller/RouteController.java
+++ src/main/java/com/incarcloud/tensoranalyzor/controller/RouteController.java
@@ -2,14 +2,30 @@ package com.incarcloud.tensoranalyzor.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
-@Controller
+import java.io.*;
+import java.util.Scanner;
+
+
+@RestController
 public class RouteController {
-    @RequestMapping(value = "/{path:TaskManager|NewAnalyzor|About}/**")
-    public String redirect() {
-        return "forward:/";
+    private final Logger s_logger = LoggerFactory.getLogger(RouteController.class);
+
+    @RequestMapping("/{path:TaskManager|NewAnalyzor|About}/**")
+    public Mono<String> SinglePageApp(@Value("${tensor-analyzor.index}") Resource html){
+        try {
+            Scanner s = new Scanner(html.getInputStream()).useDelimiter("\\A");
+            String result = s.hasNext() ? s.next() : "";
+            return Mono.just(result);
+        }
+        catch (IOException e){
+            return Mono.just(e.getMessage());
+        }
     }
 }
