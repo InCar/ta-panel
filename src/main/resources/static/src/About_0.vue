fbf209d4155935bb12cd5570f5008bbd211ee268
XGH <ghxiong@incarcloud.com>
2022-10-21 19:00:59 +0800

    mark git version

--- src/main/resources/static/src/About_0.vue
+++ src/main/resources/static/src/About_0.vue
@@ -1,3 +1,23 @@
+<style scoped lang="scss">
+    .container{
+        margin: 1em;
+        font-weight: bold;
+    }
+</style>
+
 <template>
-    
+    <div class="container">
+        {{version}}
+    </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const version = ref("");
+
+onMounted(async ()=>{
+    const resp = await fetch("/api/version");
+    version.value = await resp.text();
+})
+</script>
