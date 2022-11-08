5f632da7249951378ab88173c5d5d35ca41ee335
XGH <ghxiong@incarcloud.com>
2022-11-08 16:02:54 +0800

    splite routes

--- src/main/resources/static/src/main.ts
+++ src/main/resources/static/src/main.ts
@@ -1,38 +1,14 @@
 ﻿import './styles.scss';
 
 import { createApp, App as AppT } from 'vue';
-import { createRouter, RouteRecordRaw, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import { createPinia } from 'pinia';
+import { routes } from './routes';
 import { TensorAnalyzor } from '@remote';
 import App from './App.vue';
-import Home from './page/Home.vue';
-import TaskManager from './page/TaskManager.vue';
-    import TaskPage from "./page/TaskManager/TaskPage.vue";
-import NewAnalyzor from './page/NewAnalyzor.vue';
-import About from "./page/About.vue";
-    import About0 from "./page/About/About_0.vue";
-    import AboutDev from "./page/About/About_dev.vue";
-
-const ToDo = ()=>import('./page/todo.vue');
 
 class Main{
     private readonly app : AppT
-
-    // NOTE: 如果增加了一级路径,需要同步修改Java后端RouteController里的redirect表达式
-    private readonly routes: ReadonlyArray<RouteRecordRaw> = [
-        { path: '/', component: Home, meta: { topLevel: true, title: "分析结果", icon: "tenancy" } },
-        { path: '/TaskManager', component: TaskManager, meta: { topLevel: true, title: "任务管理", icon: "task"},
-            children: [ { path: ':taskId', component: TaskPage } ]
-        },
-        { path: '/NewAnalyzor', component: NewAnalyzor, meta: { topLevel: true, title: "创建新任务", icon: "add_box" } },
-        { path: '/About', component: About, meta: { topLevel: true, title: "About", icon: "info" },
-            children: [
-                { path: '', name: 'About', component: About0 },
-                { path: 'dev', component: AboutDev },
-            ]
-        },
-        { path: '/ToDo', component: ToDo }
-    ];
 
     public constructor(){        
         this.app = createApp(App);
@@ -46,7 +22,7 @@ class Main{
         
         const router = createRouter({
             history: createWebHistory(),
-            routes: this.routes
+            routes
         });
         this.app.use(router);
 
---
+++ src/main/resources/static/src/routes.ts
@@ -0,0 +1,26 @@
+import { RouteRecordRaw } from 'vue-router';
+
+import Home from './page/Home.vue';
+import TaskManager from './page/TaskManager.vue';
+    import TaskPage from "./page/TaskManager/TaskPage.vue";
+import NewAnalyzor from './page/NewAnalyzor.vue';
+import About from "./page/About.vue";
+    import About0 from "./page/About/About_0.vue";
+    import AboutDev from "./page/About/About_dev.vue";
+
+const ToDo = ()=>import('./page/todo.vue');
+
+export const routes: ReadonlyArray<RouteRecordRaw> = [
+    { path: '/', component: Home, meta: { topLevel: true, title: "分析结果", icon: "tenancy" } },
+    { path: '/TaskManager', component: TaskManager, meta: { topLevel: true, title: "任务管理", icon: "task"},
+        children: [ { path: ':taskId', component: TaskPage } ]
+    },
+    { path: '/NewAnalyzor', component: NewAnalyzor, meta: { topLevel: true, title: "创建新任务", icon: "add_box" } },
+    { path: '/About', component: About, meta: { topLevel: true, title: "About", icon: "info" },
+        children: [
+            { path: '', name: 'About', component: About0 },
+            { path: 'dev', component: AboutDev },
+        ]
+    },
+    { path: '/ToDo', component: ToDo }
+];
