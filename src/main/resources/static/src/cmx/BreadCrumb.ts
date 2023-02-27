c3108bb1ca85175c4aa27499a5164ffc6b153cd5
XGH <ghxiong@incarcloud.com>
2023-02-27 15:24:21 +0800

    clean all the front side files

--- src/main/resources/static/src/cmx/BreadCrumb.ts
+++
@@ -1,6 +0,0 @@
-import { Ref } from "vue";
-
-export interface BreadCrumbItem{
-    text: Ref<string>;
-    data?: any;
-}
--- src/main/resources/static/src/ta/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./TADef";
-export * from "./mode";
-export * from "./TensorAnalyzor";
-export * from "./browser/browser";
-
-export * from "./remote/BackPointDef";
-export * from "./ops"
